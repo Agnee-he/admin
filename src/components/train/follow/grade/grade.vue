9b92218691c48eb82cda61836c2c072bf016e01d
Agnee-he <15958021691@163.com>
2017-07-07 23:05:32 +0800

    11
    引入echart 。饼状图

--- src/components/train/follow/grade/grade.vue
+++ src/components/train/follow/grade/grade.vue
@@ -73,19 +73,46 @@
         // 基于准备好的dom，初始化echarts实例
         let myChart = this.$echarts.init(document.getElementById('myChart'));
         // 绘制图表
-        myChart.setOption({
-          title: { text: '' },
-          tooltip: {},
-          xAxis: {
-            data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-          },
-          yAxis: {},
-          series: [{
-            name: '销量',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
-          }]
-        });
+        myChart.setOption(
+          {
+            title: {
+              text: '某站点用户访问来源',
+              subtext: '纯属虚构',
+              x: 'center'
+            },
+            tooltip: {
+              trigger: 'item',
+              formatter: '{a} <br/>{b} : {c} ({d}%)'
+            },
+            legend: {
+              orient: 'vertical',
+              left: 'left',
+              data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+            },
+            series: [
+              {
+                name: '访问来源',
+                type: 'pie',
+                radius: '55%',
+                center: ['50%', '60%'],
+                data: [
+                  {value: 335, name: '直接访问'},
+                  {value: 310, name: '邮件营销'},
+                  {value: 234, name: '联盟广告'},
+                  {value: 135, name: '视频广告'},
+                  {value: 1548, name: '搜索引擎'}
+                ],
+                itemStyle: {
+                  emphasis: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                  }
+                }
+              }
+            ]
+          }
+        );
       }
     }
   };
@@ -119,6 +146,11 @@
         width 50px;
         height 70px;
         background-color aqua;
+    .chart
+      text-align center;
+      #myChart
+        width 300px;
+        height 300px;
     .table
       margin-top 15px;
 </style>
